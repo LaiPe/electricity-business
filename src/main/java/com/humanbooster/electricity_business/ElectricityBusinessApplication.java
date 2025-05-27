cfcb1ef3fe64f7b158cb4d7804f2a3137bcb8f35
Léo Peyronnet <peyronnet.leo@gmail.com>
2025-05-27 15:10:21 +0200

    add spring initializr boilerplate

---
+++ src/main/java/com/humanbooster/electricity_business/ElectricityBusinessApplication.java
@@ -0,0 +1,11 @@
+package com.humanbooster.electricity_business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricityBusinessApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricityBusinessApplication.class, args);
+	}
+}
